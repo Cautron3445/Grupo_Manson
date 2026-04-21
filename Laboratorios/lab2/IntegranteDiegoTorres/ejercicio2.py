ee8becb3f062f989bf89256afe291d90ff366261
diegotorresf-commits <diegotorresf@mail.pucv.cl>
2026-04-20 22:44:04 -0400

    ---

---
+++ Laboratorios/lab2/IntegranteDiegoTorres/ejercicio2.py
@@ -0,0 +1,20 @@
+coords_x = [2, 8, 8, 2]
+coords_y = [1, 1, 5, 5]
+
+# Operación entre el máximo y el mínimo de x
+operacion = max(coords_x) - min(coords_x)
+print(f"La operación (máximo - mínimo) de x es: {operacion}")
+
+# Operación entre el máximo y el mínimo de x
+operacion = max(coords_x) - min(coords_x)
+print(f"La operación (máximo - mínimo) de x es: {operacion}")
+
+# Operación entre el máximo y el mínimo de x
+operacion = max(coords_x) - min(coords_x)
+print(f"La operación (máximo - mínimo) de x es: {operacion}")
+
+# Operación entre el máximo y el mínimo de x
+operacion = max(coords_x) - min(coords_x)
+print(f"La operación (máximo - mínimo) de x es: {operacion}")
+
+def evaluar_zona_poligono(x,y):
